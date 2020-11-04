477d16c02e5eab97af8796eef07879d7820dee3f
slmgnm <54582297+slmgnm@users.noreply.github.com>
2020-11-04 10:36:10 +0200

    Add files via upload

---
+++ src/components/movies.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+  deleteHandler = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th></th>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.deleteHandler(movie)}
+                  type="button"
+                  class="btn btn-danger btn-sm">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
